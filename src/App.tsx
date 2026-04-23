10b31e908d7a2f0facac835dec1216dcba4c9388
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-04-23 18:28:39 -0300

    feat: slideshow mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "./components/header"
+import Slides from "./components/slides"
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Slides></Slides>
+
     </>
   )
 }
